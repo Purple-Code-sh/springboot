e3978ebb068528acab8323c0ddfe3e9474a7ac8b
Purple-Code-sh <purple.code.sh@gmail.com>
2025-10-03 15:29:40 -0600

    first commit

---
+++ src/main/java/com/example/demo/MyFirstClass.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyFirstClass {
+    public String sayHello(){
+        return "Hello world";
+    }
+}
