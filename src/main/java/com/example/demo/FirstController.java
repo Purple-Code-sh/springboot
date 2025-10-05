93acc2da381831748a1b16093129750c6dfec22d
Purple-Code-sh <purple.code.sh@gmail.com>
2025-10-05 16:27:41 -0600

    Clean all code from get and post method usage

--- src/main/java/com/example/demo/FirstController.java
+++ src/main/java/com/example/demo/FirstController.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,28 +11,6 @@ public class FirstController {
         return "Hello from my first controller";
     }
 
-
-    @GetMapping("/hello/{user-name}")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public String pathVar(
-            @PathVariable("user-name")
-            String userName
-    ){
-        return "the path variable is: " + userName;
-    }
-
-    @GetMapping("/hello2")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public String paramVar(
-            @RequestParam("param1")
-            String searchParam1,
-
-            @RequestParam("param2")
-            String searchParam2
-    ){
-        return "the params  are: " + searchParam1 + " and " + searchParam2;
-    }
-
     @PostMapping("/post")
     @ResponseStatus(HttpStatus.CREATED)
     public String post(
@@ -43,22 +20,5 @@ public class FirstController {
         return "Request Accepted and message is: " + message;
     }
 
-    @PostMapping("/post-order")
-    @ResponseStatus(HttpStatus.CREATED)
-    public String post(
-            @RequestBody
-            Order order
-    ){
-        return "Request Accepted and the order is: " + order.toString();
-    }
-
-    @PostMapping("/post-order-record")
-    @ResponseStatus(HttpStatus.CREATED)
-    public String postRecord(
-            @RequestBody
-            OrderRecord order
-    ){
-        return "Request Accepted and the order is: " + order.toString();
-    }
 }
 
