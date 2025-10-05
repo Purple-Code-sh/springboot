cfcac0451815338f6dbd8b324c4197b79c5fcf8b
Purple-Code-sh <purple.code.sh@gmail.com>
2025-10-05 16:24:32 -0600

    Use the get and post methods with their different use cases

--- src/main/java/com/example/demo/FirstController.java
+++ src/main/java/com/example/demo/FirstController.java
@@ -12,6 +12,28 @@ public class FirstController {
         return "Hello from my first controller";
     }
 
+
+    @GetMapping("/hello/{user-name}")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String pathVar(
+            @PathVariable("user-name")
+            String userName
+    ){
+        return "the path variable is: " + userName;
+    }
+
+    @GetMapping("/hello2")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String paramVar(
+            @RequestParam("param1")
+            String searchParam1,
+
+            @RequestParam("param2")
+            String searchParam2
+    ){
+        return "the params  are: " + searchParam1 + " and " + searchParam2;
+    }
+
     @PostMapping("/post")
     @ResponseStatus(HttpStatus.CREATED)
     public String post(
@@ -19,6 +41,24 @@ public class FirstController {
             String message
     ){
         return "Request Accepted and message is: " + message;
+    }
+
+    @PostMapping("/post-order")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String post(
+            @RequestBody
+            Order order
+    ){
+        return "Request Accepted and the order is: " + order.toString();
+    }
+
+    @PostMapping("/post-order-record")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String postRecord(
+            @RequestBody
+            OrderRecord order
+    ){
+        return "Request Accepted and the order is: " + order.toString();
     }
 }
 
