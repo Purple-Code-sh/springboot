22b75a1cdcdbee65ace21d6c427aefffbc46d558
Purple-Code-sh <purple.code.sh@gmail.com>
2025-10-08 20:19:44 -0600

    add database connection and first entity creation

---
+++ src/main/java/com/example/demo/Student.java
@@ -0,0 +1,73 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "T_STUDENT")
+public class Student {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    @Column(
+            name = "c_fname",
+            length = 80
+    )
+    private String firstname;
+    private String lastname;
+    @Column(unique = true)
+    private String email;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String firstname, String lastname, String email, int age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+
+}
