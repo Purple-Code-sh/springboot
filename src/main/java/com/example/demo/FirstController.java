d2a536c03c1bd1f838047488547b0a9da96f2522
Purple-Code-sh <purple.code.sh@gmail.com>
2025-10-08 20:47:50 -0600

    Persist entity data

--- src/main/java/com/example/demo/FirstController.java
+++ src/main/java/com/example/demo/FirstController.java
@@ -1,23 +1,26 @@
 package com.example.demo;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstController {
-    @GetMapping("/hello")
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    public String sayHello(){
-        return "Hello from my first controller";
+    private final StudentRepository studentRepository;
+
+    public FirstController(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
     }
 
-    @PostMapping("/post")
+    @PostMapping("/students")
     @ResponseStatus(HttpStatus.CREATED)
-    public String post(
+    public Student post(
             @RequestBody
-            String message
+            Student student
     ){
-        return "Request Accepted and message is: " + message;
+        return studentRepository.save(student);
     }
 
 }
